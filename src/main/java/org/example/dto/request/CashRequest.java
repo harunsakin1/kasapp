8f4d7d28793fb8dafb0e5680f480cb7f918bc3a8
harunsakin1 <harunsakin16@gmail.com>
2026-02-02 23:54:55 +0300

    Kasaya tutar giriş çıkışları eklendi. Dashboard'da günlük giriş, çıkış, aylık net ve kasadaki toplam miktar gösteriliyor. Loglama sistemi kuruldu. Pagination sistemi kuruldu.

--- src/main/java/org/example/dto/request/CashRequest.java
+++ src/main/java/org/example/dto/request/CashRequest.java
@@ -1,7 +1,12 @@
 package org.example.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+
 import java.math.BigDecimal;
 
-public record CashRequest(BigDecimal amount,
+public record CashRequest(
+                          @NotNull
+                          BigDecimal amount,
+                          @NotNull
                           String description) {
 }
